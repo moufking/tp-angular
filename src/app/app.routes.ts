afe3164e4f8fb3f8ee3ef4d6d3d7b846fc50e94b
Moufid <moufking337@gmail.com>
2018-09-06 19:08:29 +0100

    tp_angular

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import {Routes} from "@angular/router";
+import {LibraryComponent} from './library/library.component';
+import {SearchComponent} from './search/search.component';
+import {BookComponent} from './book/book.component';
+import {BookListComponent} from './book-list/book-list.component';
+
+
+export const routes: Routes = [
+
+    {path: 'library',component: LibraryComponent},
+    {path: 'book/bookid',component: BookComponent },
+    {path: 'search',component: SearchComponent },
+    {path: '', redirectTo: 'library', pathMatch: 'full'}
+
+
+
+];
