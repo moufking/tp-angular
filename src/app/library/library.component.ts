a9825279eaa838bfeeca6307bb0a5709beb69576
Moufid <moufking337@gmail.com>
2018-09-07 12:17:56 +0100

    tp_angular_bouton

---
+++ src/app/library/library.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from "@angular/core";
+import {LibraryService} from "../shared/library.service";
+
+@Component({
+  selector: 'app-library',
+  templateUrl: './library.component.html',
+  styleUrls: ['./library.component.css']
+})
+export class LibraryComponent implements OnInit {
+
+  constructor( private libraryService :LibraryService) {
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/search/search.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnInit} from '@angular/core';
+import {GoogleBooksService} from "../shared/google-books.service";
+import {Book} from "../shared/book";
+import {Router, ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  private term: string =" ";
+
+  constructor( private router :Router, private route :ActivatedRoute,
+               private googleBooksService: GoogleBooksService ) {
+    this.route.params.subscribe(params =>{
+
+      console.log(params);
+      if(params['term'])
+      {
+
+        this.term =params['term'];
+        this.onSearch( this.term);
+
+      }
+    });
+  }
+
+  doSearch() {
+    this.router.navigate(['search',{term :this.term }]);
+  }
+
+
+  onSearch (term: string) {
+    this.googleBooksService.searchBooks(term);
+
+  }
+
+  ngOnInit() {
+  }
+
+}
